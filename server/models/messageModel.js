0ead0dd72ce28f20992b2dc2331ce2f6fa6b0513
Oumar3 <oumaralitori1@gmail.com>
2025-10-13 10:45:27 +0100

    feat: add user and message model

---
+++ server/models/messageModel.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const User = require('./userModel');
+
+const messageSchema = new Schema({
+  sender: {type: Schema.Types.ObjectId, ref: 'User', required: true },
+  receiver: {type: Schema.Types.ObjectId, ref: 'User', required: true },
+  content: { type: String, required: true },
+  timestamp: { type: Date, default: Date.now }
+});
+
+const Message = mongoose.model('Message', messageSchema);
+
+module.exports = Message;
