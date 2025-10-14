a8576d43154ff648272405877c9eed6cad46144d
Oumar3 <oumaralitori1@gmail.com>
2025-10-14 13:00:14 +0100

    feat:add groupe models

---
+++ server/models/groupeModel.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const groupSchema = new Schema({
+  name: { type: String, required: true, unique: true },
+  members: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Group', groupSchema);
--- server/models/messageModel.js
+++ server/models/messageModel.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const User = require('./userModel');
+const Group = require('./groupeModel');
 
 const messageSchema = new Schema({
   senderId: {type: Schema.Types.ObjectId, ref: 'User', required: true },
   receiverId: {type: Schema.Types.ObjectId, ref: 'User', required: true },
+  groupId: {type: Schema.Types.ObjectId, ref: 'Group', required: true },
   content: { type: String, required: true },
   timestamp: { type: Date, default: Date.now }
 }, {
